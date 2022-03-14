d5c0f12ecfc698e63b6c284bdfec1a5e6a84e188
Forever <Forever@weilai.com>
2022-03-14 21:58:36 +0800

    项目初始结构创建

---
+++ livedata-collect/src/main/java/com/weiye/livedata/collect/LivedataCollectApplication.java
@@ -0,0 +1,13 @@
+package com.weiye.livedata.collect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LivedataCollectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LivedataCollectApplication.class, args);
+    }
+
+}
---
+++ livedata-monitor/src/main/java/com/weiye/livedata/monitor/LivedataMonitorApplication.java
@@ -0,0 +1,13 @@
+package com.weiye.livedata.monitor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LivedataMonitorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LivedataMonitorApplication.class, args);
+    }
+
+}
