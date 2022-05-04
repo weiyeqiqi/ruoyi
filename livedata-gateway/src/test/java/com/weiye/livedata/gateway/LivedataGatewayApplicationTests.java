21ff15f112bf03d5c212cabec5bf0e1e0d5f637c
Forever <Forever@weilai.com>
2022-05-04 20:57:45 +0800

    前台分类展示功能搭建

---
+++ livedata-gateway/src/test/java/com/weiye/livedata/gateway/LivedataGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.weiye.livedata.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LivedataGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
